9d4b8ae8ba0cc8cdbd9fb18836f981c4b6e31604
Marta Błażkowska <mablazkowska@pgs-soft.com>
2023-09-04 19:39:56 +0200

    Adds PWA

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,29 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // VitePWA({
+    //   injectRegister: "auto",
+    // }),
     VitePWA({
-      injectRegister: "auto",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "My Awesome App",
+        short_name: "MyApp",
+        description: "My Awesome App description",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "logo512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
     }),
     react(),
   ],
