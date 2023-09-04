da3268c685e471df6140fc29c4bd579cb6c70bfd
Marta Błażkowska <mablazkowska@pgs-soft.com>
2023-09-04 19:10:48 +0200

    Adds PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="h-screen bg-neutral-900">
+    <div className="h-screen bg-gray-200 dark:bg-neutral-900">
       <h1 className="text-3xl text-cyan-600 text-center py-4 px-2 subpixel-antialiased">
         Running App
       </h1>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      injectRegister: "auto",
+    }),
+    react(),
+  ],
 });
